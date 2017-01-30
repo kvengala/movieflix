6dd66f0cab446da175e7d1417e11f20aac86ffed
kvengala <kvengala@hawk.iit.edu>
2017-01-30 16:54:09 -0600

    Add web front end files

---
+++ client/routes/signin.js
@@ -0,0 +1,6 @@
+"use strict";
+function signin(req, res) {
+    res.render('signin', { title: "Logon Page" });
+}
+exports.signin = signin;
+//# sourceMappingURL=signin.js.map
---
+++ client/routes/signin.ts
@@ -0,0 +1,12 @@
+﻿/*
+*
+*GET Sign up page
+* 
+*/
+import express = require('express');
+
+export function signin(req: express.Request, res: express.Response) {
+
+    res.render('signin', { title: "Logon Page" })
+}
+
