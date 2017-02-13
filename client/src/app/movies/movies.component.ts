ef3fa8a941a791d6a1f6c30dd71ab65a57b7d9fa
kvengala <kvengala@hawk.iit.edu>
2017-02-13 02:13:17 -0600

    update movie view

---
+++ client/src/app/movies/movies.component.ts
@@ -0,0 +1,7 @@
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'login.component.html'
+})
